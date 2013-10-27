a3899b77d5d720feade2ef5b93781027bc4ce075
Thomas Wendt <thoemy@gmx.net>
2013-10-27 22:41:53 +0100

    audio: Support MR1 audio_policy
    Change-Id: If9543260bc8fb2d411a119dc62720d632eceec6c

--- include/hardware/audio_policy.h
+++ include/hardware/audio_policy.h
@@ -233,12 +233,13 @@ struct audio_policy {
     bool (*is_stream_active)(const struct audio_policy *pol,
             audio_stream_type_t stream,
             uint32_t in_past_ms);
-
+#ifndef ICS_AUDIO_BLOB
+#ifndef MR1_AUDIO_BLOB
     bool (*is_stream_active_remotely)(const struct audio_policy *pol,
             audio_stream_type_t stream,
             uint32_t in_past_ms);
+#endif
 
-#ifndef ICS_AUDIO_BLOB
     bool (*is_source_active)(const struct audio_policy *pol,
             audio_source_t source);
 #endif
